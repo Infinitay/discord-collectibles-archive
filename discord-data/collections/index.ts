a115bfbe9dba2601c8d90077d64b5a32ae30e5be
Infinitay <Infinitay@users.noreply.github.com>
2024-06-30 10:42:35 -0400

    Updated collection gen & collections data for older missing properties 	- Changed the older data reference (20231101) to be the newly converted one 	- Now sorts the collections by date (ascending) prior to generating and exporting 	- Updated the generated collection files to include the missing properties

--- discord-data/collections/index.ts
+++ discord-data/collections/index.ts
@@ -1,41 +1,41 @@
-import anime from "~discord-data/collections/anime.json" assert { type: "json" };
-import arcade from "~discord-data/collections/arcade.json" assert { type: "json" };
-import breakfast from "~discord-data/collections/breakfast.json" assert { type: "json" };
-import cyberpunk from "~discord-data/collections/cyberpunk.json" assert { type: "json" };
-import disxcore from "~discord-data/collections/disxcore.json" assert { type: "json" };
-import elements from "~discord-data/collections/elements.json" assert { type: "json" };
-import fall from "~discord-data/collections/fall.json" assert { type: "json" };
 import fantasy from "~discord-data/collections/fantasy.json" assert { type: "json" };
-import feelinRetro from "~discord-data/collections/feelin-retro.json" assert { type: "json" };
-import galaxy from "~discord-data/collections/galaxy.json" assert { type: "json" };
+import breakfast from "~discord-data/collections/breakfast.json" assert { type: "json" };
+import disxcore from "~discord-data/collections/disxcore.json" assert { type: "json" };
+import fall from "~discord-data/collections/fall.json" assert { type: "json" };
 import halloween from "~discord-data/collections/halloween.json" assert { type: "json" };
-import lofiVibes from "~discord-data/collections/lofi-vibes.json" assert { type: "json" };
-import lunarNewYear from "~discord-data/collections/lunar-new-year.json" assert { type: "json" };
-import monsters from "~discord-data/collections/monsters.json" assert { type: "json" };
-import pirates from "~discord-data/collections/pirates.json" assert { type: "json" };
-import springtoons from "~discord-data/collections/springtoons.json" assert { type: "json" };
-import uncategorized from "~discord-data/collections/uncategorized.json" assert { type: "json" };
-import valorant from "~discord-data/collections/valorant.json" assert { type: "json" };
 import winterWonderland from "~discord-data/collections/winter-wonderland.json" assert { type: "json" };
+import monsters from "~discord-data/collections/monsters.json" assert { type: "json" };
+import cyberpunk from "~discord-data/collections/cyberpunk.json" assert { type: "json" };
+import lunarNewYear from "~discord-data/collections/lunar-new-year.json" assert { type: "json" };
+import elements from "~discord-data/collections/elements.json" assert { type: "json" };
+import anime from "~discord-data/collections/anime.json" assert { type: "json" };
+import uncategorized from "~discord-data/collections/uncategorized.json" assert { type: "json" };
+import springtoons from "~discord-data/collections/springtoons.json" assert { type: "json" };
+import valorant from "~discord-data/collections/valorant.json" assert { type: "json" };
+import lofiVibes from "~discord-data/collections/lofi-vibes.json" assert { type: "json" };
+import galaxy from "~discord-data/collections/galaxy.json" assert { type: "json" };
+import feelinRetro from "~discord-data/collections/feelin-retro.json" assert { type: "json" };
+import pirates from "~discord-data/collections/pirates.json" assert { type: "json" };
+import arcade from "~discord-data/collections/arcade.json" assert { type: "json" };
 
 export default {
-	anime,
-	arcade,
-	breakfast,
-	cyberpunk,
-	disxcore,
-	elements,
-	fall,
 	fantasy,
-	feelinRetro,
-	galaxy,
+	breakfast,
+	disxcore,
+	fall,
 	halloween,
-	lofiVibes,
-	lunarNewYear,
+	winterWonderland,
 	monsters,
-	pirates,
-	springtoons,
+	cyberpunk,
+	lunarNewYear,
+	elements,
+	anime,
 	uncategorized,
+	springtoons,
 	valorant,
-	winterWonderland
+	lofiVibes,
+	galaxy,
+	feelinRetro,
+	pirates,
+	arcade
 };
