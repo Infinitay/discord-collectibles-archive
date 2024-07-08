a582a190476d7899b1b50051474f7a9527a5e83c
Infinitay <Infinitay@users.noreply.github.com>
2024-07-08 17:10:12 -0400

    Fix typing exports for Collections and ProfileEffects

--- discord-data/ProfileEffects.ts
+++ discord-data/ProfileEffects.ts
@@ -1,8 +1,12 @@
 import { ProfileEffect } from "~/types/ProfileEffects";
 import profileEffects from "./profile-effects/index";
 
+// Infer the type of the profile effects object
+type InferredProfileEffects = typeof profileEffects;
+
+// Using the inferred type, create a new type that maps the key names to the CollectiblesCategories type
 type ProfileEffectsExports = {
-	[key: string]: ProfileEffect[];
+	[K in keyof InferredProfileEffects]: ProfileEffect[];
 };
 
 const typedProfileEffects: ProfileEffectsExports = profileEffects;
