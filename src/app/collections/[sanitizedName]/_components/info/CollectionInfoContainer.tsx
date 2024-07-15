57df2dd882f53de9edef801a1364ad45ca04a9e2
Infinitay <Infinitay@users.noreply.github.com>
2024-07-15 03:43:21 -0400

    feat(frontend): More collection data, changed layout, add CollectionUtils 	- Added CollectionUtils for fetching certain collection data properties 		- Currently, added utils for Product price fetching 	- Changed the CollectionInfoContainer layout to be two halves 		- Splits basic collection information and color swatches

--- src/app/collections/[sanitizedName]/_components/info/CollectionInfoContainer.tsx
+++ src/app/collections/[sanitizedName]/_components/info/CollectionInfoContainer.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { CollectiblesCategories } from "~/types/CollectiblesCategories";
+import { CollectiblesCategories, ItemTypes } from "~types/CollectiblesCategories";
 import CollectionInfoEntry from "./CollectionInfoEntry";
+import { DiscordUtils } from "~/utils/DiscordUtils";
+import { CollectionUtils } from "~/utils/CollectionUtils";
 
 export default function CollectionInfoContainer(props: { collection: CollectiblesCategories }) {
 	// Thank you Claude 3.5 for the styling
@@ -10,10 +12,33 @@ export default function CollectionInfoContainer(props: { collection: Collectible
 		{ label: "Confetti Colors", colors: props.collection.styles.confetti_colors }
 	];
 
+	const avatarDecorationsLength = props.collection.products.filter((product) => product.type === ItemTypes.AvatarDecoration).length;
+	const profileEffectsLength = props.collection.products.filter((product) => product.type === ItemTypes.ProfileEffect).length;
+
+	const currencyFormatter = new Intl.NumberFormat("en-us", { style: "currency", currency: "USD" });
+	const { totalOriginalCost, totalDiscountedCost } = props.collection.products.reduce(
+		(acc, product) => {
+			const originalPrice = CollectionUtils.getOriginalPrice(product);
+			const discountedPrice = CollectionUtils.getNitroPrice(product);
+			return { totalOriginalCost: acc.totalOriginalCost + originalPrice, totalDiscountedCost: acc.totalDiscountedCost + discountedPrice };
+		},
+		{ totalOriginalCost: 0, totalDiscountedCost: 0 }
+	);
+	const totalCostsString = `${currencyFormatter.format(totalOriginalCost)} (${currencyFormatter.format(totalDiscountedCost)} with Nitro)`;
+
 	return (
-		<div className="mt-5 w-full max-w-[1280px] px-4">
-			<div className="mx-auto max-w-[800px]">
+		<div className="mt-5 flex w-full max-w-[1280px] px-4">
+			<div className="my-auto w-[640px]">
 				<CollectionInfoEntry name="Name" value={props.collection.name} />
+				<CollectionInfoEntry name="Description" value={props.collection.summary} />
+				<CollectionInfoEntry name="Added on" value={new Date(DiscordUtils.snowflakeToDate(props.collection.sku_id)).toLocaleString()} />
+				<CollectionInfoEntry name="SKU ID" value={props.collection.sku_id} />
+				<CollectionInfoEntry name="Total Products" value={props.collection.products.length} />
+				<CollectionInfoEntry name="Total Cost (USD)" value={totalCostsString} />
+				<CollectionInfoEntry name="Avatar Decorations" value={avatarDecorationsLength} />
+				<CollectionInfoEntry name="Profile Effects" value={profileEffectsLength} />
+			</div>
+			<div className="my-auto w-[640px]">
 				{infoItems.map((item) => (
 					<CollectionInfoEntry key={item.label} name={item.label} colors={item.colors} />
 				))}
