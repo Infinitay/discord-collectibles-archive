dccc1206597737fb2035f152f6e726d6bcd04066
Infinitay <Infinitay@users.noreply.github.com>
2024-07-17 04:31:16 -0400

    style: Add box-shadow to CollectionColors swatches

--- src/app/collections/[sanitizedName]/_components/info/CollectionColors.tsx
+++ src/app/collections/[sanitizedName]/_components/info/CollectionColors.tsx
@@ -6,7 +6,13 @@ export default function CollectionColors(props: { className?: string; colors: nu
 					.map((color) => color.toString(16).padStart(6, "0").toUpperCase())
 					.map((colorHex) => (
 						<div id={`color-${colorHex}`} key={colorHex} className="flex w-fit flex-col items-center">
-							<div className="mb-1 h-12 w-[60px] rounded-md" style={{ backgroundColor: `#${colorHex}` }}></div>
+							<div
+								className="mb-1 h-12 w-[60px] rounded-md"
+								style={{
+									backgroundColor: `#${colorHex}`,
+									boxShadow: `rgba(0, 0, 0, 0.1) 0px 0px 5px 0px, rgba(0, 0, 0, 0.1) 0px 0px 1px 0px`
+								}}
+							></div>
 							<span className="w-20 text-center">#{colorHex}</span>
 						</div>
 					))}
