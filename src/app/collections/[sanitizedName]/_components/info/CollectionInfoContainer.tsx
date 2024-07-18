910d1f164a468e3d720d106061dabee9b8124ab4
Infinitay <Infinitay@users.noreply.github.com>
2024-07-18 05:34:14 -0400

    style(Collection): Changed CollectionInfoContainer and Entry styling 	- Reduced size of CollectioninfoContainer by increasing horizontal padding 	- Apply padding within CollectionInfoContainer instead of Entry 	- Entry names are no longer centered 	- Now uses flex for each half of the CollectionInfo 	- Removed showing avatar decoration and profile effect counts

--- src/app/collections/[sanitizedName]/_components/info/CollectionInfoContainer.tsx
+++ src/app/collections/[sanitizedName]/_components/info/CollectionInfoContainer.tsx
@@ -12,9 +12,6 @@ export default function CollectionInfoContainer(props: { collection: Collectible
 		{ label: "Confetti Colors", colors: props.collection.styles.confetti_colors }
 	];
 
-	const avatarDecorationsLength = CollectionUtils.getAvatarDecorations(props.collection).length;
-	const profileEffectsLength = CollectionUtils.getProfileEffects(props.collection).length;
-
 	const currencyFormatter = new Intl.NumberFormat("en-us", { style: "currency", currency: "USD" });
 	const { totalOriginalCost, totalDiscountedCost } = props.collection.products.reduce(
 		(acc, product) => {
@@ -27,18 +24,16 @@ export default function CollectionInfoContainer(props: { collection: Collectible
 	const totalCostsString = `${currencyFormatter.format(totalOriginalCost)} (${currencyFormatter.format(totalDiscountedCost)} with Nitro)`;
 
 	return (
-		<div className="mt-5 flex w-full max-w-[1280px] px-4">
-			<div className="my-auto w-[640px]">
+		<div className="mt-5 flex w-[1280px] max-w-[1280px] px-32">
+			<div className="flex w-[640px] flex-col justify-center gap-y-6">
 				<CollectionInfoEntry name="Name" value={props.collection.name} />
 				<CollectionInfoEntry name="Description" value={props.collection.summary} />
 				<CollectionInfoEntry name="Added on" value={new Date(DiscordUtils.snowflakeToDate(props.collection.sku_id)).toLocaleString()} />
 				<CollectionInfoEntry name="SKU ID" value={props.collection.sku_id} />
 				<CollectionInfoEntry name="Total Products" value={props.collection.products.length} />
 				<CollectionInfoEntry name="Total Cost (USD)" value={totalCostsString} />
-				<CollectionInfoEntry name="Avatar Decorations" value={avatarDecorationsLength} />
-				<CollectionInfoEntry name="Profile Effects" value={profileEffectsLength} />
 			</div>
-			<div className="my-auto w-[640px]">
+			<div className="flex w-[640px] flex-col justify-center gap-y-6">
 				{infoItems.map((item) => (
 					<CollectionInfoEntry key={item.label} name={item.label} colors={item.colors} />
 				))}
