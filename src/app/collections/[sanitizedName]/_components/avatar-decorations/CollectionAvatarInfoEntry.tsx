fe7a893d17fbe246298d5d1a53856becca30dab7
Infinitay <Infinitay@users.noreply.github.com>
2024-07-18 06:04:47 -0400

    feat(frontend): Add avatar decorations to a collection's page 	- Add CollectionAvatarsContainer component to display the avatar decorations within a collection 	- Add CollectionAvatarInfoEntry component to display any avatar decoration information

---
+++ src/app/collections/[sanitizedName]/_components/avatar-decorations/CollectionAvatarInfoEntry.tsx
@@ -0,0 +1,19 @@
+import CollectionColors from "../info/CollectionColors";
+
+interface CollectionInfoEntryProps {
+	name: string;
+	value: string | number | boolean;
+}
+
+export default function CollectionInfoEntry(props: CollectionInfoEntryProps) {
+	return (
+		<div className="mb-6 flex items-start">
+			<div className="flex w-[140px] min-w-[140px] pr-4">
+				<span className="overflow-hidden break-words font-bold">{props.name}</span>
+			</div>
+			<div className="flex-grow">
+				<span className="">{props.value}</span>
+			</div>
+		</div>
+	);
+}
