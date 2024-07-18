5ef782676aa857399e7fe3365d9d22f052c5cac9
Infinitay <Infinitay@users.noreply.github.com>
2024-07-18 06:04:47 -0400

    feat(frontend): Add avatar decorations to a collection's page 	- Add CollectionAvatarsContainer component to display the avatar decorations within a collection 	- Add CollectionAvatarInfoEntry component to display any avatar decoration information

---
+++ src/app/collections/[sanitizedName]/_components/avatar-decorations/CollectionAvatarsContainer.tsx
@@ -0,0 +1,28 @@
+import { Product } from "~/types/CollectiblesCategories";
+import CollectionAvatarInfoEntry from "./CollectionAvatarInfoEntry";
+import { CollectionUtils } from "~/utils/CollectionUtils";
+
+export default function CollectionAvatarsContainer(props: { avatarDecorations: Product[] }) {
+	const currencyFormatter = new Intl.NumberFormat("en-us", { style: "currency", currency: "USD" });
+
+	return (
+		<div className="mt-5 flex w-[800px] max-w-[1280px] flex-col items-center gap-5">
+			{props.avatarDecorations.map((ad) => (
+				<div key={ad.sku_id} className="hover:shadow-2xl/50 mb-2 flex w-full bg-gray-50 shadow-md hover:bg-gray-100">
+					<div className="max-h-[240px] min-h-[240px] min-w-[240px] max-w-[240px] rounded-md">
+						<img src={CollectionUtils.getAvatarDecorationURL(ad, true)} alt={`${ad.name} Avatar Decoration`} width={240}></img>
+					</div>
+					<div className="ml-5 flex w-[320px] flex-grow flex-col items-start justify-center">
+						<CollectionAvatarInfoEntry name="Name" value={ad.name}></CollectionAvatarInfoEntry>
+						<CollectionAvatarInfoEntry name="Description" value={ad.summary}></CollectionAvatarInfoEntry>
+						<CollectionAvatarInfoEntry name="SKU ID" value={ad.sku_id}></CollectionAvatarInfoEntry>
+						<CollectionAvatarInfoEntry
+							name="Price"
+							value={currencyFormatter.format(CollectionUtils.getOriginalPrice(ad))}
+						></CollectionAvatarInfoEntry>
+					</div>
+				</div>
+			))}
+		</div>
+	);
+}
