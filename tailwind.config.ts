1cf34f964e0d18df4ce2c1dabf0393df03a70015
Infinitay <Infinitay@users.noreply.github.com>
2024-07-21 02:56:32 -0400

    styles: Add Discord Brand colors to tailwindcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,18 @@ export default {
 		extend: {
 			fontFamily: {
 				sans: ["var(--font-geist-sans)", ...fontFamily.sans]
+			},
+			colors: {
+				// Discord Brand Colors
+				"discord-blurple": "#5865F2",
+				"discord-green": "#57F287",
+				"discord-yellow": "#FEE75C",
+				"discord-fuchsia": "#EB459E",
+				"discord-red": "#ED4245",
+				"discord-white": "#FFFFFF",
+				"discord-black": "#23272A",
+				// Extra Discord Color
+				"discord-gray": "#2c2f33"
 			}
 		}
 	},
