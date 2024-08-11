d63b8db48a9456f4b452dd0b433b4ae7a4ddfd3e
Infinitay <Infinitay@users.noreply.github.com>
2024-08-11 19:48:07 -0400

    feat: Add NavBar

---
+++ src/app/_components/NavBar.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import React from "react";
+
+const PAGES = [{ id: "collections", label: "Collections", link: "/collections" }];
+
+export default function NavBar() {
+	return (
+		<nav className="fixed left-0 right-0 top-0 z-50 flex items-center bg-gray-100/50 px-6 py-2 shadow shadow-black/10 backdrop-blur-md">
+			<div className="text-4xl font-bold uppercase">Archives</div>
+			<div className="flex gap-x-5 pl-5">
+				{PAGES.map((page) => (
+					<Link key={page.id} href={page.link} className="text-xl transition-all duration-75 hover:text-black/50 hover:drop-shadow">
+						{page.label}
+					</Link>
+				))}
+			</div>
+		</nav>
+	);
+}
