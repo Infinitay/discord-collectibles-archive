d5f78f2c09e3cf7962e622f44db39a714b1320e4
Infinitay <Infinitay@users.noreply.github.com>
2024-08-12 19:23:13 -0400

    feat: Add link to 'home' and use logo instead of text

--- src/app/_components/NavBar.tsx
+++ src/app/_components/NavBar.tsx
@@ -6,7 +6,9 @@ const PAGES = [{ id: "collections", label: "Collections", link: "/collections" }
 export default function NavBar() {
 	return (
 		<nav className="fixed left-0 right-0 top-0 z-50 flex items-center bg-gray-100/50 px-6 py-2 shadow shadow-black/10 backdrop-blur-md">
-			<div className="text-4xl font-bold uppercase">Archives</div>
+			<Link href="/" className="text-4xl font-bold uppercase">
+				<img src="/icon.png" alt="Discord Collectibles Archive Logo" className="h-10 w-10 drop-shadow hover:drop-shadow-lg" />
+			</Link>
 			<div className="flex gap-x-5 pl-5">
 				{PAGES.map((page) => (
 					<Link key={page.id} href={page.link} className="text-xl transition-all duration-75 hover:text-black/50 hover:drop-shadow">
