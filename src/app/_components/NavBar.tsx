8ccf02e35d24af76890fe94e7607b231501b44d3
Infinitay <Infinitay@users.noreply.github.com>
2024-08-13 03:37:03 -0400

    style: Fix excess padding due to wrong h-screen placement

--- src/app/_components/NavBar.tsx
+++ src/app/_components/NavBar.tsx
@@ -5,7 +5,7 @@ const PAGES = [{ id: "collections", label: "Collections", link: "/collections" }
 
 export default function NavBar() {
 	return (
-		<nav className="fixed left-0 right-0 top-0 z-50 flex items-center bg-gray-100/50 px-6 py-2 shadow shadow-black/10 backdrop-blur-md">
+		<nav className="fixed top-0 z-50 flex w-full items-center bg-gray-100/50 px-6 py-2 shadow shadow-black/10 backdrop-blur-md">
 			<Link href="/" className="text-4xl font-bold uppercase">
 				<img src="/icon.png" alt="Discord Collectibles Archive Logo" className="h-10 w-10 drop-shadow hover:drop-shadow-lg" />
 			</Link>
